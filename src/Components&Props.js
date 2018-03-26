3f1022bd75b08858cbaa9eef7f5b0ec174b43186
Peilin Jiang <peilinjiang@peilins-mbp.wireless.concordia.ca>
2018-03-26 16:26:25 -0400

    add practice of components&props

--- src/Components&Props.js
+++ src/Components&Props.js
@@ -1,49 +1,3 @@
-/*import React from 'react'
-import ReactDom from 'react-dom'
-
-
-
-class Clock extends React.Component{
-	constructor(props){
-
-		super(props);
-
-		this.state ={
-			date: new Date()
-		}
-	}
-
-	componentDidMount(){
-		this.timer=setInterval(()=>this.start(),1000);
-
-	}
-
-	componentWillUnmount(){
-		clearInterval(this.timer);
-	}
-
-	start(){
-		this.setState({date: new Date()});
-	}
-
-	render(){
-		return <h1> Time is :{this.state.date.toLocaleTimeString()}</h1>
-	}
-}
-
-
-
-	ReactDom.render(
-		<Clock/>,
-		document.getElementById("root")
-	);
-
-
-
-
-
-
-*/
 import React from 'react'
 import ReactDom from 'react-dom'
 
@@ -104,7 +58,3 @@ ReactDom.render(
 	<Comment date = {comment.date} text = {comment.text} author = {comment.author}/>,
 	document.getElementById("root")
 	);
-
-
-
-
