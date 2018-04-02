0904ced9a7b61193ead950fbaa4b734caebc0888
Peilin Jiang <peilinjiang@peilins-mbp.wireless.concordia.ca>
2018-04-02 15:56:56 -0400

    add multiple inputs form practice

--- src/index.js
+++ src/index.js
@@ -197,7 +197,7 @@ ReactDom.render(
 
 	)*/
 
-const posts = [
+/*const posts = [
   {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
   {id: 2, title: 'Installation', content: 'You can install React from npm.'}
 ];
@@ -235,5 +235,104 @@ function Blog(props) {
 ReactDom.render(
 	<Blog posts = {posts} />,
 	document.getElementById('root')
+	)*/
+
+/*class EssayForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'Please write an essay about your favorite DOM element.'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('An essay was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Essay:
+          <textarea value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+
+ReactDom.render(
+	<EssayForm />,
+	document.getElementById("root")
+
+	)*/
+
+class Reservation extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			isGoing: true,
+			numberOfGuests:2
+		};
+		this.handleInputChange = this.handleInputChange.bind(this);
+	}
+
+	handleInputChange(event){
+		const target = event.target;
+		const value = target.type === 'checkbox' ? target.checked : target.value;
+		const name = target.name;
+
+		this.setState({
+			[name]:value
+		})
+	}
+
+
+	render(){
+		return(
+			<form>
+				<label>
+					Is going:
+					<input
+						name = "Going"
+						type = "checkbox"
+						checked = {this.state.isGoing}
+						onChange = {this.handleInputChange}
+					/>
+						
+				</label>
+				<br/>
+				<label>
+					Number of guests:
+					<input
+						name = "numberOfGuests"
+						type = "number"
+						value = {this.state.numberOfGuests}
+						onChange = {this.handleInputChange}
+					/>
+				</label>
+			</form>
+
+			);
+	}
+}
+
+ReactDom.render(
+	<Reservation />,
+	document.getElementById("root")
 	)
+
+
+
+
 
